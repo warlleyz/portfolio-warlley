fb56dea67ce2cef3be5c85e07936653acf33d8cc
Warlley Silva <warlleysilvax@gmail.com>
2026-09-14 11:04:18 -0300

    Cria visual base do desktop

--- src/components/Desktop/Desktop.jsx
+++ src/components/Desktop/Desktop.jsx
@@ -3,7 +3,18 @@ import './Desktop.css'
 function Desktop() {
     return (
         <main className="desktop">
-            <h1>Desktop</h1>
+            <section className="desktop-shortcuts">
+                <div className="shortcut">Sobre mim</div>
+                <div className="shortcut">Projetos</div>
+                <div className="shortcut">Tecnologias</div>
+                <div className="shortcut">Contato</div>
+                <div className="shortcut">Currículo</div>
+                <div className="shortcut">Terminal</div>
+            </section>
+
+            <div className="taskbar-placeholder">
+                Barra de tarefas
+            </div>
         </main>
     )
 }
