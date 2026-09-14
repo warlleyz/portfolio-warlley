96d497b59d2cdbbf77b813ea33b72034c11a3a99
Warlley Silva <warlleysilvax@gmail.com>
2026-09-14 11:07:25 -0300

    Adiciona icones aos atalhos do desktop

--- src/components/Desktop/Desktop.jsx
+++ src/components/Desktop/Desktop.jsx
@@ -1,15 +1,60 @@
 import './Desktop.css'
 
+import {
+    UserRound,
+    Folder,
+    Cpu,
+    Mail,
+    FileText,
+    TerminalSquare,
+} from 'lucide-react'
+
 function Desktop() {
+    const shortcuts = [
+        {
+            name: 'Sobre mim',
+            icon: <UserRound size={30} strokeWidth={1.8} />,
+        },
+        {
+            name: 'Projetos',
+            icon: <Folder size={30} strokeWidth={1.8} />,
+        },
+        {
+            name: 'Tecnologias',
+            icon: <Cpu size={30} strokeWidth={1.8} />,
+        },
+        {
+            name: 'Contato',
+            icon: <Mail size={30} strokeWidth={1.8} />,
+        },
+        {
+            name: 'Currículo',
+            icon: <FileText size={30} strokeWidth={1.8} />,
+        },
+        {
+            name: 'Terminal',
+            icon: <TerminalSquare size={30} strokeWidth={1.8} />,
+        },
+    ]
+
     return (
         <main className="desktop">
             <section className="desktop-shortcuts">
-                <div className="shortcut">Sobre mim</div>
-                <div className="shortcut">Projetos</div>
-                <div className="shortcut">Tecnologias</div>
-                <div className="shortcut">Contato</div>
-                <div className="shortcut">Currículo</div>
-                <div className="shortcut">Terminal</div>
+                {shortcuts.map((shortcut) => (
+                    <button
+                        key={shortcut.name}
+                        className="shortcut"
+                        type="button"
+                    >
+                        <span className="shortcut-icon">
+                            {shortcut.icon}
+                        </span>
+
+                        <span className="shortcut-name">
+                            {shortcut.name}
+                        </span>
+                    </button>
+                ))}
             </section>
 
             <div className="taskbar-placeholder">
